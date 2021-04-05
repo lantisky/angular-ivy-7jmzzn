d78f762e7fd0dc3db788622db7b2ac2cca6d5fb3
lantisky <lantislexc@gmail.com>
2021-04-05 13:40:13 +1200

    1 file modified

--- src/app/lib/list.reducer.ts
+++ src/app/lib/list.reducer.ts
@@ -1,10 +1,11 @@
-import { Action } from '@ngrx/store'
+import { Action } from "@ngrx/store";
 
 const initialState = {
-  Ingredients: []
+  ingredients: []
 };
-export function listReducer(state = initialState, action:Action){
-  switch(action.type){
-    case 'ADD_INGREDIENT': return{...state}; //return to the state and never change to existing state. 
-  };
+export function listReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case "ADD_INGREDIENT":
+      return { ...state, ingredients: [...state.ingredients] }; //return to the state and never change to existing state.
+  }
 }
